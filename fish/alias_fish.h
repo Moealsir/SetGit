b7f7769a93655351275258e135474daa4ae48afe
Moealsir <mohamedwdalsir@gmail.com>
2023-10-04 00:58:16 +0300

    modified

--- fish/alias_fish.h
+++ fish/alias_fish.h
@@ -3,43 +3,47 @@
 clear
 sudo mkdir ~/.config/fish/functions
  
-echo 'function e
+sudo echo 'function e
     echo "$argv[1]" > "$argv[2]"
 end
 ' >> ~/.config/fish/functions/e.fish
 funcsave e
 
-echo 'function ea
+sudo echo 'function ea
     echo "$argv[1]" >> "$argv[2]"
 end
-' >> ~/.config/fish/functions/ea.fish
+' > ~/.config/fish/functions/ea.fish
 funcsave ea
 
-echo 'function m
+sudo echo 'function m
     mkdir -vp $argv[1]
     cd $argv[1]
 end' > ~/.config/fish/functions/m.fish
 funcsave m 
 
-echo 'function gcl
+sudo echo 'function gcl
     git clone https://$token@github.com/$gituser/$argv[1].git
     cd $argv[1]
 end' > ~/.config/fish/functions/gcl.fish
 funcsave gcl 
 
-echo 'function gacp
+sudo echo 'function gacp
     git add .
     git commit -m $argv[1]
     git push
 end' > ~/.config/fish/functions/gacp.fish
 funcsave gacp 
 
-echo 'function gtc
+sudo echo 'function gtc
     git config --global user.name $gituser
     git config --global user.email $gitmail
 
     echo "Git global configuration set to Name: $gituser, Email: $gitmail, Password: $gitpass"
 end' > ~/.config/fish/functions/gtc.fish
 funcsave gtc 
+
+cat fish_aliases >> ~/.config/fish/config.fish
+clear 
+
 fish 
 echo "Scripted successfully."
